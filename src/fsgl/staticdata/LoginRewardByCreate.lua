8146d2ab096d05373ad08a2c8974d06d3b9ab637
chenjinglei123456 <60681811+chenjinglei123456@users.noreply.github.com>
2020-03-02 14:13:52 +0800

    替换前端表

--- src/fsgl/staticdata/LoginRewardByCreate.lua
+++ src/fsgl/staticdata/LoginRewardByCreate.lua
@@ -10,10 +10,10 @@ LoginRewardByCreate =
     { id = 8, needlogin = 8, reword1type = 4, reword1id = 1027, reword1num = 100, reword2type = 4, reword2id = 2317, reword2num = 1, reword3type = 4, reword3id = 2004, reword3num = 3, reword4type = 4, reword4id = 2022, reword4num = 100, },
     { id = 9, needlogin = 9, reword1type = 4, reword1id = 1027, reword1num = 200, reword2type = 4, reword2id = 2302, reword2num = 100, reword3type = 4, reword3id = 2307, reword3num = 9, reword4type = 4, reword4id = 2023, reword4num = 100, },
     { id = 10, needlogin = 10, reword1type = 4, reword1id = 1027, reword1num = 300, reword2type = 4, reword2id = 2243, reword2num = 200, reword3type = 4, reword3id = 2310, reword3num = 100, reword4type = 4, reword4id = 2021, reword4num = 100, },
-    { id = 11, needlogin = 11, reword1type = 4, reword1id = 1027, reword1num = 400, reword2type = 4, reword2id = 2305, reword2num = 10, reword3type = 4, reword3id = 2352, reword3num = 10, reword4type = 4, reword4id = 2032, reword4num = 800, },
-    { id = 12, needlogin = 12, reword1type = 4, reword1id = 1027, reword1num = 500, reword2type = 4, reword2id = 2319, reword2num = 1, reword3type = 4, reword3id = 2602, reword3num = 1, reword4type = 4, reword4id = 2034, reword4num = 800, },
-    { id = 13, needlogin = 13, reword1type = 4, reword1id = 1027, reword1num = 600, reword2type = 4, reword2id = 2303, reword2num = 10, reword3type = 4, reword3id = 2606, reword3num = 1, reword4type = 4, reword4id = 2035, reword4num = 800, },
-    { id = 14, needlogin = 14, reword1type = 4, reword1id = 1027, reword1num = 700, reword2type = 4, reword2id = 2251, reword2num = 1250, reword3type = 4, reword3id = 2607, reword3num = 1, reword4type = 4, reword4id = 2033, reword4num = 800, },
+    { id = 11, needlogin = 11, reword1type = 4, reword1id = 1027, reword1num = 400, reword2type = 4, reword2id = 2305, reword2num = 10, reword3type = 4, reword3id = 2352, reword3num = 10, reword4type = 4, reword4id = 2020, reword4num = 800, },
+    { id = 12, needlogin = 12, reword1type = 4, reword1id = 1027, reword1num = 500, reword2type = 4, reword2id = 2319, reword2num = 1, reword3type = 4, reword3id = 2602, reword3num = 1, reword4type = 4, reword4id = 2022, reword4num = 800, },
+    { id = 13, needlogin = 13, reword1type = 4, reword1id = 1027, reword1num = 600, reword2type = 4, reword2id = 2303, reword2num = 10, reword3type = 4, reword3id = 2606, reword3num = 1, reword4type = 4, reword4id = 2023, reword4num = 800, },
+    { id = 14, needlogin = 14, reword1type = 4, reword1id = 1027, reword1num = 700, reword2type = 4, reword2id = 2251, reword2num = 1250, reword3type = 4, reword3id = 2607, reword3num = 1, reword4type = 4, reword4id = 2021, reword4num = 800, },
     { id = 15, needlogin = 15, reword1type = 4, reword1id = 1027, reword1num = 1200, reword2type = 4, reword2id = 2321, reword2num = 1, reword3type = 4, reword3id = 2705, reword3num = 100, reword4type = 6, reword4id = 0, reword4num = 1000000, },
 }
 
